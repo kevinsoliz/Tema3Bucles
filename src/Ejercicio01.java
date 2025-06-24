9082403e993ed1086127ac2d51fa405aa6ccde2a
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-24 17:43:30 +0200

    Esta es mi versión, por cierto, ha ocurrido algo raro con IntelliJ, me ejecutaba el código sin modificar. Lo solucioné restaurando todo el IDE: File, Manage IDE Settings, Restore Default Settings.

---
+++ src/Ejercicio01.java
@@ -0,0 +1,39 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        byte edad = pedirEdad(sc);
+
+        byte edadMax = 0;
+        byte edadMin = 100;
+
+        while(edad != -1){
+            if(edad > edadMax)
+                edadMax = edad;
+            if(edad < edadMin)
+                edadMin = edad;
+            edad = pedirEdad(sc);
+        }
+        System.out.println("Edad Max: " + edadMax);
+        System.out.println("Edad Min: " + edadMin);
+    }
+    public static byte pedirEdad(Scanner sc){
+        byte edad = 0;
+        boolean valid = false;
+        while(!valid){
+            try {
+                System.out.println("Introduce edad: ");
+                edad = sc.nextByte();
+                valid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Número inválido");
+                sc.next();
+            }
+        }
+        return edad;
+    }
+    
+}
