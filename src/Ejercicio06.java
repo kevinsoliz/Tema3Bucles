95c3bf4c5976141bcdce66e8675a039d5e13d844
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-25 16:52:27 +0200

    Tiene que haber otra manera de hacer esto

---
+++ src/Ejercicio06.java
@@ -0,0 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        short sumaAleatoria = 0;
+        short respuesta = 0;
+
+        do {
+            byte numAle1 = generarNumAleatorio();
+            byte numAle2 = generarNumAleatorio();
+            sumaAleatoria = (short) (numAle1 + numAle2);
+            System.out.print(numAle1 + " + " + numAle2 + " = ");
+            respuesta = pedirRespuesta(sc);
+            if (verificarRespuesta(respuesta, sumaAleatoria))
+                System.out.println("Correcto!");
+            else
+                System.out.println("Incorrecto!");
+        } while (verificarRespuesta(respuesta, sumaAleatoria));
+
+    }
+
+    public static short pedirRespuesta(Scanner sc){
+        short respuesta;
+        while (true){
+            try {
+                respuesta = sc.nextShort();
+                break;
+            } catch (InputMismatchException e){
+                System.out.println("Número no válido.");
+                sc.next();
+            }
+        }
+        return respuesta;
+    }
+
+    public static byte generarNumAleatorio(){
+        return (byte)((Math.random()*100) + 1);
+    }
+
+    public static boolean verificarRespuesta(short respuesta, short sumaAleatoria){
+        return (respuesta == sumaAleatoria);
+    }
+}
