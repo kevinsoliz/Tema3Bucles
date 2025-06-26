ccf87600c8d9ddb843e6eb6029c7c0569415ef99
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-26 11:32:34 +0200

    numero > 0 era la clave para no mostrar el 0

--- src/Ejercicio13.java
+++ src/Ejercicio13.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
+
 public class Ejercicio13 {
     public static void main(String[] args) {
-        short numero = 123;
-        byte guarismo = (byte)(numero % 10);
-        numero = (short)(numero / 10);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero: ");
 
-        do{
-            System.out.println("El guarismo es: " + guarismo);
-            guarismo = (byte)(numero % 10);
+        short numero = sc.nextShort();
 
+        while (numero > 0){
+            byte guarismo = (byte) (numero % 10);
+            System.out.println(guarismo);
             numero /= 10;
-        }while (guarismo != 0);
+        }
     }
 }
