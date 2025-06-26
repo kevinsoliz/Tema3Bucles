4a20cb31ea911c093b7b1091261d1d5c4d599201
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-26 11:06:11 +0200

    Mucho mejor: impar = i * 2 - 1

--- src/Ejercicio12.java
+++ src/Ejercicio12.java
@@ -2,9 +2,8 @@ public class Ejercicio12 {
     public static void main(String[] args) {
         short suma = 0;
 
-        for (int i = 1; i <= 20; i++){
-             if (!(i % 2 == 0))
-                suma += i;
+        for (int i = 1; i <= 10; i++){
+             suma += (short)(i * 2 - 1);
         }
         System.out.println("Suma: " + suma);
     }
