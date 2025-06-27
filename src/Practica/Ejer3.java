d6c7d7eb9025e8bfeb4c695e07c5b82b063ae7be
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-27 12:02:29 +0200

    Lo tengo habia que poner un solo espacio en el primer bucle

--- src/Practica/Ejer3.java
+++ src/Practica/Ejer3.java
@@ -11,7 +11,7 @@ public class Ejer3 {
         for (int i = 0; i < numFilasYColumnas; i++){
 
             for (int j = i; j < numFilasYColumnas; j++){
-                System.out.print("  ");
+                System.out.print(" ");
             }
             for (int k = 0; k <= i; k++){
                 System.out.print("* ");
